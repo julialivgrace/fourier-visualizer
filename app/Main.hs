e4ce6f779f819487765315828da96ef1afc9d29a
Julia DeFoor <juliagracedefoor@gmail.com>
2020-07-28 17:51:47 -0400

    Improve readability of Arrows.hs functions
    More complex functions were reduced into multiple smaller functions. The
    arrow data class was replaced with a simple type definition. I bundled
    rotation speed and initial position in a new Seed type instead of in the
    Arrow data type.

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,11 @@
 module Main where
 
 import Arrow
+import Data.Complex
 import Graphics.Gloss
+
+inits :: Seed
+inits = [(s, 100 :+ 50) | s <- [-10 .. 10]]
 
 main :: IO ()
 main = animate FullScreen (greyN 0.05) (drawArrows inits)
--- src/Arrow.hs
+++ src/Arrow.hs
@@ -2,37 +2,44 @@ module Arrow where
 
 import Data.Complex
 import Graphics.Gloss
-import Graphics.Gloss.Data.Vector
-import qualified Graphics.Gloss.Data.Point.Arithmetic as A
 
-data Arrow = Arrow {speed :: Integer, initialPosition :: Vector}
+type Arrow = Complex Float
 
-inits :: [Arrow]
-inits = [Arrow s (50, 50) | s <- [-10 .. 10]]
+-- A seed is a list of rotation speeds and their corresponding arrow's starting positions
+type Seed = [(Float, Arrow)]
 
-vectorize :: Complex Float -> Vector
-vectorize c = (realPart c, imagPart c)
+rotateArrow :: Float -> Arrow -> Arrow
+rotateArrow angle = (*) (cis angle)
 
-positionAt :: Float -> Arrow -> Vector
-positionAt t arrow = rotateV angle . initialPosition $ arrow
+vectorize :: Arrow -> Vector
+vectorize a = (realPart a, imagPart a)
+
+updateArrow :: Float -> Float -> Arrow -> Arrow
+updateArrow t speed = rotateArrow (t * speed)
+
+updateSeed :: Float -> Seed -> [Arrow]
+updateSeed t = map (uncurry $ updateArrow t)
+
+-- Create a path through the arrows arranged tip to tail
+arrange :: [Arrow] -> Path
+arrange = map vectorize . scanl (+) (0 :+ 0)
+
+arrowsAt :: Float -> Seed -> Path
+arrowsAt t = arrange . updateSeed t
+
+-- Create a path through the arranged arrows' endpoints at the given times
+trace :: [Float] -> Seed -> Path
+trace ts seed = map endpoint ts
   where
-    angle = fromInteger (speed arrow) * t
+    endpoint t = last (arrowsAt t seed)
 
-arrowPath :: Float -> [Arrow] -> Path
-arrowPath t = scanl (A.+) (0, 0) . map (positionAt t)
-
-trailPath :: [Float] -> [Arrow] -> Path
-trailPath ts inits = map endpointAt ts
-  where
-    endpointAt t = last (arrowPath t inits)
-
-drawArrows :: [Arrow] -> Float -> Picture
-drawArrows inits seconds = Pictures [vectors, tip, trail]
+drawArrows :: Seed -> Float -> Picture
+drawArrows seed seconds = Pictures [vectors, tip, trail]
   where
     t = seconds / 5
-    positions = arrowPath t inits
+    positions = arrowsAt t seed
     (x, y) = last positions
     -- draw stuff
     vectors = color (light aquamarine) $ line positions
-    trail = color white $ line (trailPath [0, 0.01 .. t] inits)
+    trail = color white $ line (trace [0, 0.01 .. t] seed)
     tip = color red $ translate x y $ circleSolid 5
