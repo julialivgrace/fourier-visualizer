c61d1bf04315bdca89157898bf2c1c1d284b1863
Julia DeFoor <juliagracedefoor@gmail.com>
2020-07-29 07:36:02 -0400

    Improve Arrow.hs readability further
    This should be the last edit to Arrow.hs for a while. I love how well
    these last changes turned out. "arrange (arrowsAt seed t)" is concise,
    readable, and clarifying. Good code.

--- src/Arrow.hs
+++ src/Arrow.hs
@@ -5,7 +5,7 @@ import Graphics.Gloss
 
 type Arrow = Complex Float
 
--- A seed is a list of rotation speeds and their corresponding arrow's starting positions
+-- A seed is a list of arrows and their corresponding rotation speeds
 type Seed = [(Arrow, Float)]
 
 rotateArrow :: Arrow -> Float -> Arrow
@@ -14,26 +14,23 @@ rotateArrow arrow angle = cis angle * arrow
 vectorize :: Arrow -> Vector
 vectorize a = (realPart a, imagPart a)
 
-updateSeed :: Seed -> Float -> [Arrow]
-updateSeed seed t = map (\(arrow, speed) -> rotateArrow arrow (speed * t)) seed
+arrowsAt :: Seed -> Float -> [Arrow]
+arrowsAt seed t = map (\(arrow, speed) -> rotateArrow arrow (speed * t)) seed
 
 -- Create a path through the arrows arranged tip to tail
 arrange :: [Arrow] -> Path
 arrange = map vectorize . scanl (+) (0 :+ 0)
 
-arrowsAt :: Seed -> Float -> Path
-arrowsAt seed t = arrange (updateSeed seed t)
-
-endpoint :: Seed -> Float -> Vector
-endpoint seed t = last (arrowsAt seed t)
+endpoint :: Seed -> Float -> Point
+endpoint seed = last . arrange . arrowsAt seed
 
 drawArrows :: Seed -> Float -> Picture
 drawArrows seed seconds = Pictures [vectors, tip, trail]
   where
     t = seconds / 5
-    positions = arrowsAt seed t
+    positions = arrange (arrowsAt seed t)
     (x, y) = endpoint seed t
     -- draw stuff
     vectors = color (light aquamarine) $ line positions
-    trail = color white $ line (map (endpoint seed) [0, 0.01 .. t])
     tip = color red $ translate x y $ circleSolid 5
+    trail = color white $ line (map (endpoint seed) . takeWhile (>=0) $ [t, t - 0.01 .. t - 0.05])
