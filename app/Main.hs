3b3f42d56633cead6305a05c4a700724aa2e9c60
Julia DeFoor <juliagracedefoor@gmail.com>
2020-07-29 16:07:27 -0400

    Create more seeds, add readability

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
-import           Arrow
-import           Seed
-import           Fourier
+import           Seed                           ( Seed(..)
+                                                , createSeed
+                                                , drawSeed
+                                                )
 import           Data.Complex
 import           Graphics.Gloss
 
@@ -14,9 +15,26 @@ circleSeed = createSeed f n spaceScale timeScale
   spaceScale = 100
   timeScale  = 1
 
-inits :: Seed
-inits = Seed { arrows = arrows, spaceScale = 1, timeScale = 0.01 }
-  where arrows = [ (50 :+ 50, s) | s <- [-10 .. 10] ]
+lineSeed :: Seed
+lineSeed = createSeed f n spaceScale timeScale
+ where
+  f t = (cos (2 * pi * t) :+ 0) * (1 :+ 1)
+  n          = 101
+  spaceScale = 100
+  timeScale  = 0.5
+
+rodFunction :: Float -> Complex Float
+rodFunction x | x < 0.5   = -1
+              | x > 0.5   = 1
+              | otherwise = 0.5
+
+rodSeed :: Seed
+rodSeed = createSeed rodFunction n spaceScale timeScale
+ where
+  n          = 20
+  spaceScale = 500
+  timeScale  = 0.1
+
 
 main :: IO ()
-main = animate FullScreen (greyN 0.05) (drawSeed circleSeed)
+main = animate FullScreen (greyN 0.05) (drawSeed rodSeed)
--- src/Seed.hs
+++ src/Seed.hs
@@ -1,9 +1,12 @@
 module Seed where
 
-import           Arrow
-import           Fourier
-import           Graphics.Gloss
+import           Arrow                          ( Arrow
+                                                , arrange
+                                                , endpoint
+                                                )
+import qualified Fourier
 import           Data.Complex
+import           Graphics.Gloss
 
 -- A seed is a list of arrows and their corresponding frequencies, as well as scaling information
 data Seed = Seed { arrows :: [(Arrow, Integer)], spaceScale :: Arrow, timeScale :: Float }
@@ -14,26 +17,26 @@ createSeed f n spaceScale timeScale = Seed { arrows     = arrows
                                            , timeScale  = timeScale
                                            }
  where
+  -- arrow frequencies are ordered, in cycles / second, 0, 1, -1, 2, -2, 3, -3, etc.
   freqs  = take n (0 : concat [ [x, -x] | x <- [1 ..] ])
-  cs     = map (coefficient f) freqs
+  cs     = map (Fourier.coefficient f) freqs
   arrows = zip cs freqs
 
 arrowsAt :: Seed -> Float -> [Arrow]
 arrowsAt seed t = map updateArrow (arrows seed)
  where
   updateArrow (arrow, freq) =
-    spaceScale seed
-      * cis (2 * pi * fromInteger freq * timeScale seed * t)
-      * arrow
+    spaceScale seed * cis (2 * pi * fromInteger freq * t) * arrow
 
 drawSeed :: Seed -> Float -> Picture
-drawSeed seed t = Pictures [vectors, tip, trail]
+drawSeed seed seconds = Pictures [vectors, tip, trail]
  where
+  t         = seconds * timeScale seed
   positions = arrange $ arrowsAt seed t
   (x, y)    = last positions
   -- draw stuff
   vectors   = color (light aquamarine) . line $ positions
   tip       = color red . translate x y $ circleSolid 5
-  -- draw path through through the endpoints at t, t - 1/100, t - 2/100, t - 3/100, etc.
-  ts        = takeWhile (>= 0) . map (\x -> t - x / 100) $ [0 .. 80]
   trail     = color white . line $ map (endpoint . arrowsAt seed) ts
+  -- trail passes through through the endpoints at t, t - 1/100, t - 2/100, t - 3/100, etc.
+  ts        = takeWhile (> 0) . map (\x -> t - x / 100) $ [0 .. 80]
