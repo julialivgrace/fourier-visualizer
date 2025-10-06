3b1cc50247a27635a0123737330f6171730932dd
julialivgrace <julialivgrace@gmail.com>
2025-10-05 23:15:48 -0400

    Remove extraneous import statement

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 
-import           Data.Complex
-import qualified ExampleSeeds                  as E
-import           Graphics.Gloss
-import           Seed                           ( drawSeed )
+import qualified ExampleSeeds as E
+import Graphics.Gloss
+import Seed (drawSeed)
 
 main :: IO ()
 main = animate FullScreen (greyN 0.05) (drawSeed E.heartSeed)
